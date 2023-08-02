9d4b360b02a057be037f1440b4b3fcefd7f4be82
Wandati <Petersimon801@gmail.com>
2023-08-02 22:17:48 +0300

    new restaurant component

---
+++ src/components/Restaurants.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+export default function Restaurants() {
+  const [Restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/restaurants")
+      .then((res) => res.json())
+      .then((data) => setRestaurants(data));
+  }, []);
+  const restaurants = Restaurants.map((rest) => {
+    return (
+      <div key={rest.id} className="col-6">
+        <div className="card">
+          <img
+            src={rest.image}
+            className="card-img-top img-fluid"
+            alt={rest.name}
+          />
+        </div>
+        <div className="card-body">
+          <h5 className="card-title">{rest.name}</h5>
+          <p className="card-text">Contact us on: {rest.contact} </p>
+          <button className="btn btn-dark">
+            Click Here to View Our latest Menu
+          </button>
+        </div>
+      </div>
+    );
+  });
+  return (
+    <>
+      <h1 className="text-center ">Our Restaurants</h1>
+      <div className="row mt-3">{restaurants}</div>
+    </>
+  );
+}
