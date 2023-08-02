102f8a9f8770685b5279c07ac2be4ce5fac6312c
duan spot <brandennjonge@gmail.com>
2023-08-03 01:23:10 +0300

    form submission

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,53 +1,79 @@
 import { useEffect } from "react";
-const Login = ({person,setPerson}) => {
-    useEffect(() =>{
-    fetch("http://localhost:3000",{
-        method:'POST',
-        headers:{
 
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-        },
-        body: JSON.stringify(setPerson(person)),
+const Login = ({ person, setPerson, email, setEmail }) => {
+  useEffect(() => {
+    fetch("http://localhost:3000/users", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify(person), // Send the person object directly
+    });
+  }, []);
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("form function .....");
+
+    // Create the user object with email and password
+    const user = {
+      email: email,
+      password: event.target.inputPassword.value, // Get the password from the input field
+    };
+
+    // Now you can use this "user" object to send the data to the server
+    fetch("http://localhost:3000/users", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify(user),
     })
-    
+    .then(response => response.json())
+    .then(data => {
+      console.log("User data sent successfully:", data);
+      // Do something with the response if needed, or handle successful login
+    })
+    .catch(error => {
+      console.error("Error sending user data:", error);
+      // Handle any errors that occurred during the fetch
+    });
+  }
 
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    setEmail(event.target.value);
+  }
 
-
-    },[])
-    
-    const handleSubmit=(event) =>{
-        event.preventDefault();
-        console.log("form function .....");
-
-
-
-    }
-
-    return ( 
-
-<div className="center-container">
-        <form className="small-form" onSubmit={handleSubmit}>
-            <div className="form-group row">
-                <label htmlFor="staticEmail" className="col-sm-2 col-form-label">Email</label>
-                <div className="col-sm-10">
-                    <input type="text" className="form-control-plaintext" id="staticEmail" placeholder="email@example.com"/>
-                </div>
-            </div>
-            <div className="form-group row">
-                <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Password</label>
-                <div className="col-sm-10">
-                    <input type="password" className="form-control" id="inputPassword" placeholder="Password"/>
-                </div>
-            </div>
-
-            <button type="submit" className="btn btn-primary">Login</button>
-        </form>
-
-</div>
-
-     );
+  return (
+    <div className="center-container">
+      <h3>Hello: {email} </h3>
+      <form className="small-form" onSubmit={handleSubmit}>
+        <div className="form-group row">
+          <label htmlFor="staticEmail" className="col-sm-2 col-form-label">Email</label>
+          <div className="col-sm-10">
+            <input
+              type="text"
+              className="form-control-plaintext"
+              id="staticEmail"
+              placeholder="email@example.com"
+              onChange={handleChange}
+              value={email}
+            />
+          </div>
+        </div>
+        <div className="form-group row">
+          <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Password</label>
+          <div className="col-sm-10">
+            <input type="password" className="form-control" id="inputPassword" placeholder="Password" />
+          </div>
+        </div>
+        <button type="submit" className="btn btn-primary">Login</button>
+      </form>
+    </div>
+  );
 }
- 
+
 export default Login;
--- src/components/Start.js
+++ src/components/Start.js
@@ -3,12 +3,13 @@ import { useState } from "react";
 
 const Start = () => {
 const[person,setPerson]=useState({})
+const[email,setEmail]=useState("");
     return (
         
         <>
         <p>Hallo Home </p>
         <img  className="img_main"   src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fblue-texture&psig=AOvVaw39n9EWKA2mEnNWhu5YzQSw&ust=1691061661273000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCKjJ7t7tvYADFQAAAAAdAAAAABAE" alt="half_image" />
-        <Login person={person} setPerson={setPerson}/>
+        <Login person={person} setPerson={setPerson} email={email} setEmail={setEmail}/>
         </>  
     
     );
