6783c8d5772280a66af5be43df93fd60afe16c25
Wandati <Petersimon801@gmail.com>
2023-08-02 15:01:07 +0300

    edit states

--- src/App.js
+++ src/App.js
@@ -4,22 +4,20 @@ import Footer from "./components/Footer";
 import Foods from "./components/Foods";
 function App() {
   const [Restaurants, setRestaurants] = useState([]);
-
-  // const [Foods, setFoods] = useState([]);
+  const [foods, setFoods] = useState([]);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:3000/restaurants")
       .then((res) => res.json())
       .then((data) => setRestaurants(data));
   }, []);
-  // useEffect(() => {
-  //   fetch("http://localhost:3000/food")
-  //     .then((res) => res.json())
-  //     .then((data) => setFoods(data));
-  // }, []);
-  // function handleChange(food){
 
-  // }
+  useEffect(() => {
+    fetch("http://localhost:3000/food")
+      .then((res) => res.json())
+      .then((data) => setFoods(data));
+  }, []);
 
   const restaurants = Restaurants.map((rest) => {
     return (
@@ -41,33 +39,16 @@ function App() {
       </div>
     );
   });
+  const filteredFoods = foods.filter((food) => {
+    return food.food.toLowerCase().includes(query.toLowerCase());
+  });
 
-  // const foods = Restaurants.map((rest) => {
-  //   return (
-  //     <div key={rest.id} className="col-6">
-  //       <div className="card ">
-  //         <img
-  //           src={rest.image}
-  //           className="card-img-top img-fluid"
-  //           alt={rest.name}
-  //         />
-  //       </div>
-  //       <div className="card-body">
-  //         <h5 className="card-title">{rest.name}</h5>
-  //         <p className="card-text">Contact us on: {rest.contact} </p>
-  //         <button className="btn btn-dark">
-  //           Click Here to View Our latest Menu
-  //         </button>
-  //       </div>
-  //     </div>
-  //   );
-  // });
   return (
     <>
-      <Navbar />
+      <Navbar setQuery={setQuery} />
       <div className="container mt-5">
         <h1 className="text-center">Our Foods</h1>
-        <Foods />
+        <Foods filteredFoods={filteredFoods} />
         <h1 className="text-center ">Our Restaurants</h1>
         <div className="row mt-3">{restaurants}</div>
       </div>
