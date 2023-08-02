8c0450286cc4e5b820e2913148fc69828c2d1635
Wandati <Petersimon801@gmail.com>
2023-08-02 10:46:27 +0300

    update

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 function App() {
   const [Restaurants, setRestaurants] = useState([]);
+  // const [Foods, setFoods] = useState([]);
+
   useEffect(() => {
     fetch("http://localhost:3000/restaurants")
       .then((res) => res.json())
       .then((data) => setRestaurants(data));
   }, []);
+  // useEffect(() => {
+  //   fetch("http://localhost:3000/food")
+  //     .then((res) => res.json())
+  //     .then((data) => setFoods(data));
+  // }, []);
+
   const restaurants = Restaurants.map((rest) => {
     return (
       <div key={rest.id} className="col-6">
@@ -28,6 +36,26 @@ function App() {
       </div>
     );
   });
+  // const foods = Restaurants.map((rest) => {
+  //   return (
+  //     <div key={rest.id} className="col-6">
+  //       <div className="card ">
+  //         <img
+  //           src={rest.image}
+  //           className="card-img-top img-fluid"
+  //           alt={rest.name}
+  //         />
+  //       </div>
+  //       <div className="card-body">
+  //         <h5 className="card-title">{rest.name}</h5>
+  //         <p className="card-text">Contact us on: {rest.contact} </p>
+  //         <button className="btn btn-dark">
+  //           Click Here to View Our latest Menu
+  //         </button>
+  //       </div>
+  //     </div>
+  //   );
+  // });
   return (
     <>
       <Navbar />
