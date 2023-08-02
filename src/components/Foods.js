c1e4c8e8c1b8253d193f0acd49cd6c079fc84c76
Wandati <Petersimon801@gmail.com>
2023-08-02 15:00:39 +0300

    filtering foods

--- src/components/Foods.js
+++ src/components/Foods.js
@@ -1,17 +1,11 @@
 import React from "react";
-import { useEffect, useState } from "react";
-export default function Foods() {
-  const [foods, setFoods] = useState([]);
-  useEffect(() => {
-    fetch("http://localhost:3000/food")
-      .then((res) => res.json())
-      .then((data) => setFoods(data));
-  }, []);
-  const Foods = foods.map((food) => {
+
+export default function Foods({ filteredFoods }) {
+  const Foods = filteredFoods.map((food) => {
     return (
       <div key={food.id} className="col-3">
         <div className="card h-100 p=">
-          <img src={food.image} class="card-img-top" alt={food.name} />
+          <img src={food.image} className="card-img-top" alt={food.name} />
           <div className="card-body">
             <h5 className="card-title">{food.food}</h5>
             <p className="card-text">
