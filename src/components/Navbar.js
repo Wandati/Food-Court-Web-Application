151948f8d563101d11fe8ee453cc6d0f8b9d7043
Wandati <Petersimon801@gmail.com>
2023-08-04 11:52:14 +0300

    Link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
-export default function Navbar({ query, setQuery }) {
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+export default function Navbar() {
   return (
     <nav
       className="navbar bg-dark border-bottom border-body text-white"
@@ -6,7 +7,8 @@ export default function Navbar({ query, setQuery }) {
     >
       <div className="container-fluid">
         <h1 className="navbar-brand">NEXT GEN FOOD COURT APP</h1>
-        <form className="d-flex" role="search">
+
+        {/* <form className="d-flex" role="search">
           <input
             className="form-control me-2"
             type="search"
@@ -15,9 +17,20 @@ export default function Navbar({ query, setQuery }) {
             placeholder="Search Foods here"
             aria-label="Search"
           />
-        </form>
+        </form> */}
 
-        <div className="text-white text-decoration-none">View Cart</div>
+        <Link className="text-decoration-none" to="/">
+          <div className="text-white text-decoration-none">Home</div>
+        </Link>
+        <Link className="text-decoration-none" to="/Login">
+          <div className="text-white text-decoration-none">Login</div>
+        </Link>
+        <Link className="text-decoration-none" to="/Restaurants">
+          <div className="text-white ">Our Restaurants</div>
+        </Link>
+        <Link className="text-decoration-none" to="/Cart">
+          <div className="text-white text-decoration-none">View Cart</div>
+        </Link>
       </div>
     </nav>
   );
