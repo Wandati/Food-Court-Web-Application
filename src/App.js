faafce049d643025e009bccd3f454a49b42317b8
Wandati <Petersimon801@gmail.com>
2023-08-04 11:49:27 +0300

    routes

--- src/App.js
+++ src/App.js
@@ -70,22 +70,116 @@
 //   return <Restaurants />;
 // }
 // export default App;
-import { Route, Routes } from "react-router-dom";
+// import { BrowserRouter as Route, Routes } from "react-router-dom";
+// import Start from "./components/Start";
+// import Home from "./components/Home";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       {/* all routes to be contained here */}
+//       <Routes>
+//         {/* this is the root path */}
+//         <Route path="/" element={<Start />} /> {/* 👈 Renders at /app/ */}
+//         {/* Add a new route for the Home component */}
+//         <Route path="/Home" element={<Home />} />
+//       </Routes>
+//     </div>
+//   );
+// }
+
+// export default App;
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { useState, useEffect } from "react";
+// import Navbar from "./components./Navbar";
+// import Footer from "./components./Footer";
+// import Foods from "./components./Foods";
+// import Cart from "./components./Cart";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Foods from "./components/Foods";
+import Cart from "./components/Cart";
+import Restaurants from "./components/Restaurants";
+// import Login from "./components/Login";
 import Start from "./components/Start";
-import Home from "./components/Home";
 
 function App() {
+  // const [Restaurants, setRestaurants] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
+  const [query, setQuery] = useState("");
+  const [foods, setFoods] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/food")
+      .then((res) => res.json())
+      .then((data) => setFoods(data));
+  }, []);
+  const filteredFoods = foods.filter((food) => {
+    return food.food.toLowerCase().includes(query.toLowerCase());
+  });
+
+  const addToCart = (food, quantity) => {
+    const item = { ...food, quantity };
+    setCartItems((prevCartItems) => [...prevCartItems, item]);
+  };
+
+  const removeFromCart = (id) => {
+    setCartItems((prevCartItems) =>
+      prevCartItems.filter((item) => item.id !== id)
+    );
+  };
+
+  const getTotalAmount = () => {
+    return cartItems.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    );
+  };
+
+  const clearCart = () => {
+    setCartItems([]);
+  };
+
   return (
-    <div className="App">
-      {/* all routes to be contained here */}
-      <Routes>
-        {/* this is the root path */}
-        <Route path="/" element={<Start />} /> {/* 👈 Renders at /app/ */}
-        {/* Add a new route for the Home component */}
-        <Route path="/Home" element={<Home />} />
-      </Routes>
-    </div>
+    <>
+      <Router>
+        <div>
+          <Navbar setQuery={setQuery} />
+          <Switch>
+            <Route exact path="/">
+              <div className="container mt-5">
+                <h1 className="text-center">
+                  WELCOME TO NEXTGEN FOOD COURT APP{" "}
+                </h1>
+                <h2 className="text-center">Place Your Order(s) Here</h2>
+                <Foods
+                  addToCart={addToCart}
+                  filteredFoods={filteredFoods}
+                  setQuery={setQuery}
+                />
+                {/* <h1 className="text-center ">Our Restaurants</h1>
+        <div className="row mt-3">{restaurants}</div> */}
+              </div>
+            </Route>
+            <Route exact path="/Cart">
+              <Cart
+                cartItems={cartItems}
+                removeFromCart={removeFromCart}
+                getTotalAmount={getTotalAmount}
+                clearCart={clearCart}
+              />
+            </Route>
+            <Route exact path="/Login">
+              <Start />
+            </Route>
+            <Route exact path="/Restaurants">
+              <Restaurants />
+            </Route>
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
+    </>
   );
 }
-
 export default App;
